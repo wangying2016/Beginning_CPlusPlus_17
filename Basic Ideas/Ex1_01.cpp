0f4325c02ff7fd9d1e0cc0bc474cf00f0d08c0fb
Wang Ying <wy867504146@outlook.com>
2018-07-12 22:33:06 +0800

    Chapter Ex1_01.cpp

---
+++ Basic Ideas/Ex1_01.cpp
@@ -0,0 +1,15 @@
+// Ex1_01.cpp
+// A complete C++ program
+
+#include <iostream>
+
+int main()
+{
+	int answer{ 42 };
+	
+	std::cout << "The answer to life, the universe, and everything is "
+		<< answer
+		<< std::endl;
+	
+	return 0;
+}
